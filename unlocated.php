f56f699c44e9acb5fa580ddf1b9a12c0fcc9afab
Peter Pierre <binh@sunrise.vn>
2018-07-21 11:15:19 +0700

    check if no connection

--- unlocated.php
+++ unlocated.php
@@ -3,7 +3,7 @@
     error_reporting(E_ERROR | E_PARSE);
     $config = json_decode(file_get_contents('config.json'));
     $conn = new mysqli($config->servername, $config->username, $config->password, $config->dbname);
-    if ($conn->connect_error) die();
+    if ($conn->connect_error) die('error');
 
     $sql = "SELECT id, address, postal_code FROM organisation__organisation WHERE type_clinic = true";
     $res = $conn->query($sql);
